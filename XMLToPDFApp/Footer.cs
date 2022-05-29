0adf8e007085aafef0ed28bd6c56234ceee77296
Christian Cano <christian_k106@hotmail.com>
2022-05-28 22:39:04 -0500

    1.0.2 release

--- XMLToPDFApp/Footer.cs
+++ XMLToPDFApp/Footer.cs
@@ -17,29 +17,36 @@ namespace XMLToPDFApp
         {
             Font font = new(Font.FontFamily.UNDEFINED, 7.5f);
 
-            PdfPTable tbFooter = new(6);
-
-            tbFooter.TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+            PdfPTable tbFooter = new(6)
+            {
+                TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin
+            };
             tbFooter.DefaultCell.Border = 0;
 
-            PdfPCell _cell = new(new Paragraph(footerText, font));
-            _cell.HorizontalAlignment = Element.ALIGN_LEFT;
-            _cell.Border = 0;
-            _cell.Colspan = 5;
-            _cell.PaddingBottom = 10;
+            PdfPCell _cell = new(new Paragraph(footerText, font))
+            {
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Border = 0,
+                Colspan = 5,
+                PaddingBottom = 10
+            };
             tbFooter.AddCell(_cell);
 
-            _cell = new(new Paragraph("Página " + writer.CurrentPageNumber + "/" + writer.PageNumber, font));
-            _cell.HorizontalAlignment = Element.ALIGN_RIGHT;
-            _cell.Border = 0;
+            _cell = new(new Paragraph("Página " + writer.CurrentPageNumber + "/" + writer.PageNumber, font))
+            {
+                HorizontalAlignment = Element.ALIGN_RIGHT,
+                Border = 0
+            };
             tbFooter.AddCell(_cell);
 
             string datetimeFileGeneration = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
 
-            _cell = new(new Paragraph(datetimeFileGeneration, font));
-            _cell.HorizontalAlignment = Element.ALIGN_CENTER;
-            _cell.Border = 0;
-            _cell.Colspan = 6;
+            _cell = new(new Paragraph(datetimeFileGeneration, font))
+            {
+                HorizontalAlignment = Element.ALIGN_CENTER,
+                Border = 0,
+                Colspan = 6
+            };
             tbFooter.AddCell(_cell);
 
             tbFooter.WriteSelectedRows(0, -1, document.LeftMargin, writer.PageSize.GetBottom(document.BottomMargin) + 35, writer.DirectContent);
